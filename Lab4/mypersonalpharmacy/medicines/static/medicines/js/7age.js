51f4cd867b6250af5d13d7dc493662ea6717662f
Elizabeth <bellator.379@gmail.com>
2023-11-04 22:38:46 +0300

    Seventh task with age done

---
+++ Lab4/mypersonalpharmacy/medicines/static/medicines/js/7age.js
@@ -0,0 +1,47 @@
+function calculateAgeAndCheckPermission() {
+        // Запрашиваем дату рождения
+        const dobString = prompt("Введите свою дату рождения в формате ГГГГ-ММ-ДД", "2000-01-01");
+
+        // Преобразуем введенную строку в объект Date
+        const dob = new Date(dobString);
+
+        // Рассчитываем возраст
+        const age = calculateAge(dob);
+
+        // Определяем день недели
+        const dayOfWeek = getDayOfWeek(dob);
+
+        // Проверяем, является ли пользователь совершеннолетним
+        if (age >= 18) {
+            alert(`Вам ${age} лет. День недели вашего рождения: ${dayOfWeek}`);
+        } else {
+            // Пользователь несовершеннолетний
+            const parentalConsent = confirm("Вы несовершеннолетний. Для продолжения использования сайта необходимо разрешение родителей. Продолжить?");
+            if (parentalConsent) {
+                alert(`Вам ${age} лет. День недели вашего рождения: ${dayOfWeek}. Разрешение родителей получено.`);
+            } else {
+                alert("Извините, доступ запрещен без разрешения родителей.");
+                // Дополнительные действия для блокировки доступа
+            }
+        }
+    }
+
+    function calculateAge(dateOfBirth) {
+        const today = new Date();
+        let age = today.getFullYear() - dateOfBirth.getFullYear();
+        const monthDiff = today.getMonth() - dateOfBirth.getMonth();
+
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dateOfBirth.getDate())) {
+            age--;
+        }
+
+        return age;
+    }
+
+    function getDayOfWeek(date) {
+        const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        return daysOfWeek[date.getDay()];
+    }
+
+    // Запуск функции при загрузке страницы
+    calculateAgeAndCheckPermission();
